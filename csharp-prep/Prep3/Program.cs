5e910348c75d8295c1c800bea24496d38174aa04
mholmes755 <hol23026@byui.edu>
2023-09-20 20:46:09 -0600

    Prep 3 Work

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    // Find a random # between 1 and 11
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,11);
+        int guess = -1;
+        while (guess != magicNumber)
+        {   Console.Write("Guess a number between 1 and 11: ");
+            guess = int.Parse(Console.ReadLine());
+
+        // Start conditionals to see if they guessed it right
+            if (guess != magicNumber)
+                if (guess > magicNumber)
+                {Console.WriteLine("Guess a little lower. ");}
+                else if (guess < magicNumber)
+                {Console.WriteLine("Guess a little higher ");}
+        } Console.WriteLine("That's Correct! ");
     }
 }
