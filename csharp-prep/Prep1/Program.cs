42ac2a5a96ad851e8fd1815a8465429dce6e8eb9
mholmes755 <hol23026@byui.edu>
2023-09-20 19:03:34 -0600

    Prep 1 work

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+    // Get the first name
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
+    // Get the last name
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
+    // Write names
         Console.Write($"Your name is {last_name}, {first_name} {last_name}");
 
     }
