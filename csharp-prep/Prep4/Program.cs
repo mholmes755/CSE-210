7d184d51c39530fd95f355a451e87162631ee0ca
mholmes755 <hol23026@byui.edu>
2023-09-20 22:15:50 -0600

    Prep 4 Work

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.ComponentModel;
+using System.Globalization;
 
-class Program
+List<int> numbers = new List<int>();
+int number = 1;
+Console.WriteLine("In this program we will create a list of numbers. Please enter various numbers. When you are ready to stop, input the number 0.");
+while (number != 0)
 {
-    static void Main(string[] args)
+    Console.WriteLine("Enter a number: ");
+    number = int.Parse(Console.ReadLine());
+    numbers.Add(number);
+}
+int sum = 0;
+double avg = 0.0;
+int largest = -1000;
+foreach (int num in numbers)
+{
+    sum += num;
+    avg = (double)sum / numbers.Count;
+    if (num > largest)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        largest = num;
     }
 }
+// Output of sum, largest number, and entire list
+Console.WriteLine("List of Numbers: ");
+foreach (int num in numbers)
+{ Console.WriteLine(num); }
+Console.WriteLine($"The Sum is: {sum} ");
+Console.WriteLine($"The Average is: {avg:F2} ");
+Console.WriteLine($"The largest number is: {largest} ");
