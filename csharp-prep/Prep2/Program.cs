babe747b36188c8d06939ecf4ae4efcc6c81714c
mholmes755 <hol23026@byui.edu>
2023-09-20 19:19:01 -0600

    Prep 2 Work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+    // Ask for grade and store as a variable
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        
+    // Change string variable to an integer variable
+        int number = int.Parse(grade);
+
+        if (number >= 90) 
+        {Console.WriteLine("You have an A in the class. ");}
+
+        else if (number >= 80 && number <90)
+        {Console.WriteLine("You have a B in the class. ");}
+
+        else if (number >= 70 && number <80)
+        {Console.WriteLine("You have a C in the class. ");}
+
+        else if (number >= 60 && number <70)
+        {Console.WriteLine("You have a D in the class. ");}
+
+        else if (number <60)
+        {Console.WriteLine("You have an F in the class. ");}
+
+        
     }
 }
