853ebb06e0a47bd7f09606cf21c54ac7441093a9
mholmes755 <hol23026@byui.edu>
2023-10-12 21:11:24 -0600

    Learn 05 work

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Thread.Sleep(2000);
+
+        Square _square = new Square("red", 2.5);
+        Rectangle _rectangle = new Rectangle("white", 3, 6.5);
+        Circle _circle = new Circle("blue", 5.4);
+
+        // double squareArea = _square.GetArea();
+        // string squareColor = _square.GetColor();
+
+
+        // double rectangleArea = _rectangle.GetArea();
+        // string rectangleColor = _rectangle.GetColor();
+
+
+        // double circleArea = _circle.GetArea();
+        // string circleColor = _circle.GetColor();
+
+
+
+
+        List<Shape> _shapes = new List<Shape>();
+
+        _shapes.Add(_square);
+        _shapes.Add(_rectangle);
+        _shapes.Add(_circle);
+
+        foreach (Shape s in _shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"This is a {color} shape with an area of {area}");
+
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,26 @@
+public class Shape
+{
+    protected string _color;
+
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+
+    }
+
+    protected void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0.0;
+    }
+}
